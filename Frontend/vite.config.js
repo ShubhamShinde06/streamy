bc76a63c0bb412b7f1daae47216e52780a7a85a7
ShubhamShinde06 <officialshubhamorg@gmail.com>
2025-04-01 10:31:25 +0530

    addon backend url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:8000",
+      "/api": "https://streamy-backend.onrender.com",
     },
   },
   plugins: [react(), tailwindcss()],
